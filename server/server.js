8e4d5f2b92e535551d3b5fe1295e985b89c712e1
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-17 22:13:35 -0500

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -17,9 +17,17 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (err) => {
-    res.status(400).send(err)
+    res.status(400).send(err);
   })
 });
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
