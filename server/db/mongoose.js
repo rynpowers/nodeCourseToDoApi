fd22142cfca5e0b021e81848eb0700d186495501
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-18 19:44:30 -0500

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
-mongoose.connect('mongodb://rynpowers:Ronaldo01.@ds261917.mlab.com:61917/todoapp')
+mongoose.connect('mongodb://rynpowers:Ronaldo01.@ds261917.mlab.com:61917/todoapp', {useMongoClient: true})
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 module.exports = {mongoose};
