6ad30e8a6bcfc9984778f6f2ae416b6ef35b9b37
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-20 08:52:43 -0500

    Updated PATCH route tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -7,10 +7,13 @@ const {Todo} = require('./../models/todos');
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  completed: false
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 123
 }];
 
 beforeEach((done) => {
@@ -143,97 +146,64 @@ describe('DELETE /todos/:id', () => {
 });
 
 describe('PATCH /todos/:id', () => {
-  it('should update todos {title,completed,completedAt}', (done) => {
-// change title: "Updated", completed: true, completedAt: !null
+  it('should update text, completed and completedAT', (done) => {
+
     var id = todos[0]._id.toHexString();
-    var todo = {
-      text: "Updated",
-      completed: true
-    }
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        text: "Updated",
+        completed: true
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).not.toBe('number');
+        expect(res.body.todo.text).toBe("Updated");
+      })
+      .end(done);
+  });
+  it('should update completed to false and reset completedAt to null', (done) => {
+
+    var id = todos[1]._id.toHexString();
 
     request(app)
     .patch(`/todos/${id}`)
-    .send(todo)
+    .send({
+      completed: false
+    })
     .expect(200)
     .expect((res) => {
-      expect(res.body.todo.completedAt).not.toBe(null);
-      expect(res.body.todo.completed).toBe(todo.completed);
-      expect(res.body.todo.title).toBe(todo.title);
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toBe(null);
     })
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
-      todo = {
-        completed: false
-      }
-// flip completed back to false trigger completedAt: null, title unchanged
-      request(app)
-      .patch(`/todos/${id}`)
-      .send(todo)
-      .expect((res) => {
-        expect(res.body.todo.completed).toBe(todo.completed);
-        expect(res.body.todo.completedAt).toBe(null);
-        expect(res.body.todo.title).toBe(todo.title);
-      }).then(() => {
-        todo = {
-          completed: true
-        }
-  // flip completed back to true trigger completedAt: back !null, title unchanged
-        request(app)
-        .patch(`/todos/${id}`)
-        .send(todo)
-        .expect((res) => {
-          expect(res.body.todo.completed).toBe(todo.completed);
-          expect(res.body.todo.completedAt).not.toBe(null);
-        }).then(() => done()).catch((e) => done(e));
-      });
-    });
+    .end(done)
+  })
+
+  it('should respond with 404 status', (done) => {
+
+    var id = '123'
+
+    request(app)
+    .patch(`/todos/${id}`)
+    .send({
+      text: 'New text'
+    })
+    .expect(404)
+    .end(done)
   });
 
-  it('should update todos {title}', (done) => {
+  it('should respond with 404 status', (done) => {
 
-// change only title text everything remains the same
-    var id = todos[0]._id.toHexString();
-    var todo = {
-      text: "Changed todo title",
-    }
+    var id = '5a628b744256c76c0d6ad509'
 
     request(app)
     .patch(`/todos/${id}`)
-    .send(todo)
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.todo.completedAt).toBe(null);
-      expect(res.body.todo.title).toBe(todo.title);
-      expect(res.body.todo.completed).toBe(false);
+    .send({
+      text: 'New text'
     })
-    .end(done);
-  })
-  it('should respond with 404 status', (done) => {
-
-    var id = '123';
-    var todo = {
-      text: "Changed todo title",
-    }
-
-    request(app)
-    .patch(`/todos/${id}`)
-    .send(todo)
     .expect(404)
-    .end(done);
-  })
-  it('should respond with 404 status', (done) => {
-
-    var id = '5a628b744256c76c0d6ad509';
-    var todo = {
-      text: "Changed todo title",
-    }
-
-    request(app)
-    .patch(`/todos/${id}`)
-    .send(todo)
-    .expect(404)
-    .end(done);
+    .end(done)
   })
 });
