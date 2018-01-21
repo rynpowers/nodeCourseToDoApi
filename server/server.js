573b81e8abac3cdde4f03d4a7480725e3afc10e2
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-21 09:28:48 -0500

    Add Sser POST route

--- server/server.js
+++ server/server.js
@@ -79,6 +79,18 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send());
 });
 
+// User routes
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ["email", "password"])
+
+  var user = new User(body)
+
+  user.save().then((doc) => {
+    res.send(doc)
+  }).catch((e) => res.status(400).send(e));
+});
+
 app.listen(port, () => {
   console.log('Server running on port '+ port);
 });
