62ed0eb73aa4e42ce7662b823b12a812487ef69d
Ryan Powers <danger_powers@Ryans-MacBook-Pro.local>
2018-01-22 13:12:50 -0500

    Add hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc';
 
-var data = {
-  id: 10
-}
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
-console.log('TOKEN');
-console.log(token);
-console.log('DECODED');
-console.log(decoded);
+var hashedPassword = '$2a$10$kke0bayLy80CVFmGCS/auOVfwzIqNg4aFQKDDeV86R64i3JA4SU2q'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
+// console.log('TOKEN');
+// console.log(token);
+// console.log('DECODED');
+// console.log(decoded);
 
 // jwt.verify
 
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 var userSchema = new mongoose.Schema({
@@ -65,6 +66,19 @@ userSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 }
+
+userSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            next()
+        });
+      });
+  }else {
+    next()
+  }
+});
 
 var User = mongoose.model('User', userSchema);
 
